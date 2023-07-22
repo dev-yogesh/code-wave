dd3f5436d0b3968e729659b5f7912e1fb0f63b67
dev-yogesh <yogeshgupta279@gmail.com>
2023-07-22 11:12:16 +0530

    add: home page initial implementaion

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,32 @@
+import React from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+
 export default function Home() {
+
+  const navigate = useNavigate();
+
+  const handleJoin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    navigate('/editor/123', {
+      state: {
+        username: 'Yogesh'
+      }
+    });
+  }
+
   return (
-    <div>Home Page</div>
+    <div>
+      <form noValidate onSubmit={handleJoin}>
+        <img src="" alt="logo" />
+        <label htmlFor="room_id">Paste invitaion ROOM ID</label>
+        <input id='room_id' type="text" placeholder='ROOM ID' />
+        <input type="text" placeholder='USERNAME' />
+        <button type='submit'>Join</button>
+
+        <p>If you don't have an invite then create <span>new room</span></p>
+      </form>
+
+      <footer>Built by <Link to='/'>Yogesh</Link></footer>
+    </div>
   )
 }
